7f20dbe0c08c350cd8979678a3a0e782444600f1
Aqib-Shoaib <118466147+Aqib-Shoaib@users.noreply.github.com>
2024-03-26 01:57:22 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
+  }
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+
+  // const step = 1;
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            {/* numbers */}
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          {/* message */}
+          <p className="message">{messages[step - 1]}</p>
+          {/* buttons */}
+          <div className="buttons">
+            <button onClick={handlePrevious}>Previous</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
